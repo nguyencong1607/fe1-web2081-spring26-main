33c99b121cf671198a68160da3df335111c127a0
nguyencong1607 <cong01tt@gmail.com>
2026-03-10 13:05:59 +0700

    update project

---
+++ src/app/app.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {
+  title = 'my-app';
+  fullName = 'Angular 15';
+  age = 10;
+
+  sayHello(){
+    console.log('hello');
+  }
+}
