dbb741536a6e7ec993641f7f7fd99775134dc71f
nguyencong1607 <cong01tt@gmail.com>
2026-03-14 13:12:16 +0700

    Lap3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { About } from './pages/about/about';
 import { Contact } from './pages/contact/contact';
 import { Products } from './pages/products/products';
 import { ProductDetail } from './pages/product-detail/product-detail';
+import { Stories } from './pages/stories/stories';
 
 export const routes: Routes = [  
     { path: 'home', component: Home },
     {path : 'about', component: About},
     {path : 'contact', component: Contact},
     {path: 'products', component: Products},
-    {path:'product/:id', component:ProductDetail}
+    {path:'product/:id', component:ProductDetail},
+    {path: 'stories', component:Stories}
      ];
 
---
+++ src/app/pages/stories/stories.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stories',
+  imports: [],
+  templateUrl: './stories.html',
+  styleUrl: './stories.css',
+})
+export class Stories {
+  stories = [
+  {
+    title: 'One Piece',
+    author: 'Eiichiro Oda',
+    views: 100000,
+    since: 1999,
+    category: 'Adventure',
+    image: 'https://cdn.myanimelist.net/images/manga/2/253146.jpg'
+  },
+  {
+    title: 'Naruto',
+    author: 'Masashi Kishimoto',
+    views: 90000,
+    since: 1999,
+    category: 'Action',
+    image: 'https://cdn.myanimelist.net/images/manga/3/249658.jpg'
+  },
+  {
+    title: 'Doraemon',
+    author: 'Fujiko F Fujio',
+    views: 70000,
+    since: 1969,
+    category: 'Comedy',
+    image: 'https://cdn.myanimelist.net/images/manga/1/259070.jpg'
+  },
+  {
+    title: 'Dragon Ball',
+    author: 'Akira Toriyama',
+    views: 100000,
+    since: 1984,
+    category: 'Action',
+    image: 'https://static.flixzone.co/7a/i/f/48/67664bc20b8ae.jpg'
+  },
+  {
+    title: 'Attack On Titan',
+    author: 'Hajime Isayama',
+    views: 90000,
+    since: 2009,
+    category: 'Dark Fantasy',
+    image: 'https://cdn.myanimelist.net/images/manga/2/37846.jpg'
+  },
+  {
+    title: 'Bleach',
+    author: 'Tite Kubo',
+    views: 70000,
+    since: 2001,
+    category: 'Supernatural',
+    image: 'https://animotaku.fr/wp-content/uploads/2022/12/anime-Bleach-Thousand-Year-Blood-War-Partie-2-The-Separation-visuel-2.jpeg'
+  }
+];
+}
